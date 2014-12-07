82b264578ed6ac9ff3297bd3ce6eaf573fdff099
Torsten Raudssus <torsten@raudss.us>
2014-12-07 18:52:12 +0100

    First release

---
+++ lib/DDC.pm
@@ -0,0 +1,31 @@
+package DDC;
+# ABSTRACT: Visualize random bytes
+
+use strict;
+use warnings;
+use parent 'Data::Coloured';
+
+our @EXPORT_OK = qw();
+
+our @EXPORT = qw( pc c coloured poloured );
+
+1;
+
+=head1 DESCRIPTION
+
+See L<Data::Coloured>
+
+=head1 SUPPORT
+
+IRC
+
+  Join #vonbienenstock on irc.freenode.net. Highlight Getty for fast reaction :).
+
+Repository
+
+  http://github.com/Getty/p5-data-coloured
+  Pull request and additional contributors are welcome
+
+Issue Tracker
+
+  http://github.com/Getty/p5-data-coloured/issues
--- t/load.t
+++ t/load.t
@@ -4,9 +4,10 @@ use warnings;
 use Test::More;
 
 for (qw(
-
+  Data::Coloured
+  DDC
 )) {
-use_ok($_);
+  use_ok($_);
 }
 
 done_testing;
